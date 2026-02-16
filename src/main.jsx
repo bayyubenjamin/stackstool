22cb07f336a12d814d81cf6c14bd57d91ed921ca
bayyubenjamin <bayyujack@gmail.com>
2026-02-16 10:20:11 +0700

    fix: add polyfills implementation for wallet connection

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,6 @@
+// Tambahkan baris ini di paling atas!
+import './polyfills' 
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
--- src/polyfills.js
+++ src/polyfills.js
@@ -1,6 +1,6 @@
 import { Buffer } from 'buffer';
 
-// Wajib untuk Stacks.js di Vite
+// Wajib untuk Stacks.js di Vite agar tidak error "Buffer is not defined"
 window.global = window;
 window.Buffer = Buffer;
 window.process = { env: { NODE_ENV: 'development' } };
